a5b34f9fa8a852b81ca68346d913cdebf1e9f9c9
cirodil <cirodil@gmail.com>
2022-06-24 13:15:34 +0300

    add Deta base post temp

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 // install express with `npm install express`
 const express = require("express");
-const app = express();
+const { Deta } = require("deta");
 
+const deta = Deta(process.env.SECRET_KEY);
+const db = deta.Base("Sensor");
+
+const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.post("/temp", function (req, res) {
+app.post("/temp", async function (req, res) {
   if (!req.body) return res.sendStatus(400);
 
-  let temp = req.body;
-  res.json(temp);
+  const { temp } = req.body;
+  const toCreate = { temp };
+  const insertedTemp = await db.put(toCreate);
+
+  res.status(201).json(insertedTemp);
 });
 
 app.listen(3000, () => {
